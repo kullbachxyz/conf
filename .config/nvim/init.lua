3016cb3cf3b1e33b06da445aef9f08c8f9db05f6
kullbachxyz <philipp@kullbach.net>
2026-02-18 00:39:13 +0100

    switched to lazy plugin manager; removed legacy commands

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,79 +2,76 @@
 
 vim.g.mapleader = " "
 
--- Auto-install vim-plug if missing
-local plug_path = vim.fn.stdpath('data') .. '/site/autoload/plug.vim'
-if vim.fn.empty(vim.fn.glob(plug_path)) > 0 then
-  vim.fn.system({'curl', '-fLo', plug_path, '--create-dirs',
-    'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'})
-  vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+-- ===== lazy.nvim bootstrap =====
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- vim-plug block
-vim.cmd [[
-  call plug#begin('~/.local/share/nvim/plugged')
-  Plug 'norcalli/nvim-colorizer.lua'
-  call plug#end()
-]]
-
--- Truecolor for nice colors & plugins
-vim.opt.termguicolors = true
+require("lazy").setup({
+  { "NvChad/nvim-colorizer.lua", opts = {} },
+})
 
 -- ===== Sensible defaults =====
 local o = vim.opt
 
+-- Include Vim's spell files (e.g. from vim-spell-de pacman package)
+vim.opt.rtp:append("/usr/share/vim/vimfiles")
+
+-- Truecolor
+o.termguicolors = true
+
 -- Line numbers
-o.number = true            -- absolute line number on the current line
-o.relativenumber = true    -- relative numbers on the other lines
+o.number         = true   -- absolute line number on the current line
+o.relativenumber = true   -- relative numbers on the other lines
 
 -- Indentation
-o.expandtab = true         -- tabs -> spaces
-o.shiftwidth = 2           -- indentation size
-o.tabstop = 2              -- how many spaces a <Tab> shows as
-o.smartindent = true
+o.expandtab  = true  -- tabs -> spaces
+o.shiftwidth = 2     -- indentation size
+o.tabstop    = 2     -- how many spaces a <Tab> shows as
 
--- UI tweaks
-o.cursorline = true        -- highlight current line
-o.wrap = true              -- soft wrapping
-o.scrolloff = 4            -- keep some context lines above/below cursor
-o.signcolumn = "yes"       -- always show sign column
+-- UI
+o.cursorline  = true   -- highlight current line
+o.wrap        = true   -- soft wrapping
+o.scrolloff   = 4      -- keep context lines above/below cursor
+o.signcolumn  = "yes"  -- always show sign column
+o.splitright  = true   -- vsplit opens right
+o.splitbelow  = true   -- split opens below
 
 -- Search
 o.ignorecase = true
-o.smartcase = true
-o.hlsearch = false
+o.smartcase  = true
+o.hlsearch   = false
 
 -- Misc
-o.mouse = "a"              -- enable mouse
-o.clipboard = "unnamedplus" -- use system clipboard
+o.mouse      = "a"             -- enable mouse
+o.clipboard  = "unnamedplus"   -- use system clipboard
+o.undofile   = true            -- persistent undo across sessions
+o.updatetime = 250             -- faster CursorHold
 
 -- ===== Transparency =====
--- This assumes your terminal emulator itself is set to be transparent
-vim.cmd [[
-  augroup TransparentBackground
-    autocmd!
-    " main window
-    autocmd ColorScheme * highlight Normal       ctermbg=none guibg=none
-    " floating windows
-    autocmd ColorScheme * highlight NormalFloat  ctermbg=none guibg=none
-    " sign column (git signs, diagnostics, etc.)
-    autocmd ColorScheme * highlight SignColumn   ctermbg=none guibg=none
-  augroup END
-]]
+-- Assumes your terminal emulator is set to be transparent
+local function set_transparent_bg()
+  vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "SignColumn",  { bg = "none" })
+end
 
--- apply once on startup too (in case no :colorscheme is run)
-vim.cmd [[
-  highlight Normal       ctermbg=none guibg=none
-  highlight NormalFloat  ctermbg=none guibg=none
-  highlight SignColumn   ctermbg=none guibg=none
-]]
+vim.api.nvim_create_autocmd("ColorScheme", { callback = set_transparent_bg })
+set_transparent_bg()
 
 -- ===== Spellcheck =====
 -- Enable for prose filetypes automatically
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "text", "gitcommit", "tex" },
   callback = function()
-    vim.opt_local.spell = true
+    vim.opt_local.spell     = true
     vim.opt_local.spelllang = "de,en_us"
   end,
 })
@@ -87,12 +84,8 @@ vim.keymap.set("n", "<leader>st", function()
   vim.opt_local.spell = not vim.opt_local.spell:get()
 end, { desc = "Toggle spellcheck" })
 
-vim.keymap.set("n", "<leader>sn", "]s",  { remap = true, desc = "Next misspelling" })
-vim.keymap.set("n", "<leader>sp", "[s",  { remap = true, desc = "Prev misspelling" })
-vim.keymap.set("n", "<leader>ss", "z=",  { remap = true, desc = "Spell suggestions" })
-vim.keymap.set("n", "<leader>sa", "zg",  { remap = true, desc = "Add word to dictionary" })
-vim.keymap.set("n", "<leader>sw", "zw",  { remap = true, desc = "Mark word as wrong" })
-
--- ===== Colorizer =====
-pcall(function() require("colorizer").setup() end)
-
+vim.keymap.set("n", "<leader>sn", "]s", { remap = true, desc = "Next misspelling" })
+vim.keymap.set("n", "<leader>sp", "[s", { remap = true, desc = "Prev misspelling" })
+vim.keymap.set("n", "<leader>ss", "z=", { remap = true, desc = "Spell suggestions" })
+vim.keymap.set("n", "<leader>sa", "zg", { remap = true, desc = "Add word to dictionary" })
+vim.keymap.set("n", "<leader>sw", "zw", { remap = true, desc = "Mark word as wrong" })
