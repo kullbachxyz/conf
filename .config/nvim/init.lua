2f678e93f81af83648e1fa587a1601b2c18186d1
kullbachxyz <philipp@kullbach.net>
2026-02-18 00:31:41 +0100

    vim spellcheck

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
 -- ~/.config/nvim/init.lua
 
+vim.g.mapleader = " "
+
 -- Auto-install vim-plug if missing
 local plug_path = vim.fn.stdpath('data') .. '/site/autoload/plug.vim'
 if vim.fn.empty(vim.fn.glob(plug_path)) > 0 then
@@ -33,7 +35,7 @@ o.smartindent = true
 
 -- UI tweaks
 o.cursorline = true        -- highlight current line
-o.wrap = false             -- no soft wrapping
+o.wrap = true              -- soft wrapping
 o.scrolloff = 4            -- keep some context lines above/below cursor
 o.signcolumn = "yes"       -- always show sign column
 
@@ -66,6 +68,30 @@ vim.cmd [[
   highlight NormalFloat  ctermbg=none guibg=none
   highlight SignColumn   ctermbg=none guibg=none
 ]]
+
+-- ===== Spellcheck =====
+-- Enable for prose filetypes automatically
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text", "gitcommit", "tex" },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "de,en_us"
+  end,
+})
+
+-- Single custom spellfile so zg always adds to one place regardless of language
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/custom.utf-8.add"
+
+-- Remapped for German keyboard (avoids AltGr combos for [ ] and Shift+0 for =)
+vim.keymap.set("n", "<leader>st", function()
+  vim.opt_local.spell = not vim.opt_local.spell:get()
+end, { desc = "Toggle spellcheck" })
+
+vim.keymap.set("n", "<leader>sn", "]s",  { remap = true, desc = "Next misspelling" })
+vim.keymap.set("n", "<leader>sp", "[s",  { remap = true, desc = "Prev misspelling" })
+vim.keymap.set("n", "<leader>ss", "z=",  { remap = true, desc = "Spell suggestions" })
+vim.keymap.set("n", "<leader>sa", "zg",  { remap = true, desc = "Add word to dictionary" })
+vim.keymap.set("n", "<leader>sw", "zw",  { remap = true, desc = "Mark word as wrong" })
 
 -- ===== Colorizer =====
 pcall(function() require("colorizer").setup() end)
---
+++ .config/nvim/keybinds.md
@@ -0,0 +1,17 @@
+# Neovim Keybindings
+
+Leader key: `Space`
+
+## Spellcheck
+
+> Active automatically for: markdown, text, gitcommit, tex
+> Language: German + English (`de, en_us`)
+
+| Key            | Action                    |
+|----------------|---------------------------|
+| `<leader>st`   | Toggle spellcheck         |
+| `<leader>sn`   | Next misspelling          |
+| `<leader>sp`   | Previous misspelling      |
+| `<leader>ss`   | Suggest corrections       |
+| `<leader>sa`   | Add word to dictionary    |
+| `<leader>sw`   | Mark word as wrong        |
