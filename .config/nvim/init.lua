2618a3c39fb4c0b152f40a277ab6694c8a6978d4
kullbachxyz <ph@kullbach.net>
2026-06-20 14:27:31 +0200

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,17 +3,29 @@ vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = false
 
+vim.opt.title = true
+vim.opt.background = "light"
+vim.opt.hlsearch = false
+vim.cmd("colorscheme vim")
+
 vim.opt.wrap = true
 vim.opt.linebreak = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 vim.g.mapleader = "  "
 vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>q', ':wq<CR>')
-vim.keymap.set('n', '<leader>s', ':so<CR>')
+vim.keymap.set('n', '<leader>s', ':so<CR>') -- Source current file
+vim.keymap.set('n', '<leader>e', '<cmd>!chmod +x %<CR>') -- Make current file executable
+vim.keymap.set('n', '<leader>r', '<cmd>!%:p<CR>') -- Run current file
 
 vim.opt.mouse = "a" -- Enable mouse usage
+
+-- Spellcheck
 vim.opt.spell = true
 vim.opt.spelllang = { "en", "de" }
+vim.api.nvim_set_hl(0, "SpellBad", { cterm = { underline = true }, ctermfg = 1 })
 
 vim.api.nvim_set_hl(0, "Comment", { ctermfg = 7 })
 
@@ -28,3 +40,4 @@ vim.opt.scrolloff = 8
 
 vim.opt.undofile = true
 vim.opt.swapfile = false
+
